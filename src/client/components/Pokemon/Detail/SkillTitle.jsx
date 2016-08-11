9315c5252584cefedd829dffc5e49c5b197cd607
Nandi Wong <nandi0315@gmail.com>
2016-08-11 18:41:21 +0800

    Fix summary layout issue

--- src/client/components/Pokemon/Detail/SkillTitle.jsx
+++ src/client/components/Pokemon/Detail/SkillTitle.jsx
@@ -17,7 +17,7 @@ class Skill extends React.Component {
 				<Col md={4} xs={4}><span className='summary-skill-title'>{type === "fast" ? "FAST" : "SPECIAL"}</span></Col>
 				<Col md={2} xs={2}><span className='summary-skill-title'>TYPE</span></Col>
 				<Col md={2} xs={2}><span className='summary-skill-title'>DPS</span></Col>
-				<Col md={1} xs={2}><span className='summary-skill-title'>DMG</span></Col>
+				<Col md={2} xs={2}><span className='summary-skill-title'>DMG</span></Col>
 				<Col md={2} xs={2}><span className='summary-skill-title'>{type === "fast" ? "CD" : "DUR"}</span></Col>
 			</Row>
 		);			
